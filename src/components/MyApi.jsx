7656af1a84fa0e2ec18ed50a0f8b0177f757ab66
RianLimeira <rianlimeira@gmail.com>
2023-05-27 10:57:09 -0300

    busca de dados da api

--- src/components/MyApi.jsx
+++ src/components/MyApi.jsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react"
+
+const tarefas = [
+    { id: '1', title: 'minha 1 tarefa' },
+    { id: '2', title: 'minha 2 tarefa' },
+    { id: '3', title: 'minha 3 tarefa' },
+    { id: '4', title: 'minha 4 tarefa' },
+]
+
 export default function MyApi() {
-    const tarefas = [
-        { id: '1', title: 'minha 1 tarefa' },
-        { id: '2', title: 'minha 2 tarefa' },
-        { id: '3', title: 'minha 3 tarefa' },
-        { id: '4', title: 'minha 4 tarefa' },
-    ]
+    
+
+    const [tarefas, setTarefas] = useState([])
+
+    useEffect(() => {
+        async function buscaDados() {
+            const result = await fetch('https://jsonplaceholder.typicode.com/todos')
+            const resultFinish = await result.json()
+            return resultFinish
+        }
+
+        buscaDados().then(res => setTarefas(res))
+    }, [])
 
     return (<div>
         <ol>
