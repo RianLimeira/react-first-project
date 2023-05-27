04dfcf724ddf67c7d489742fce7aa85c04bbc746
RianLimeira <rianlimeira@gmail.com>
2023-05-27 10:32:32 -0300

    update list

--- src/components/MyList.jsx
+++ src/components/MyList.jsx
@@ -1,18 +1,44 @@
+import { useEffect, useState } from "react"
+
 const myList = [
-    {id: '1', value:'Fruta'},
-    {id: '2', value:'Legumes'},
-    {id: '3', value:'Carne'},
-    {id: '4', value:'Doces'},
+    { id: '1', value: 'Fruta' },
+    { id: '2', value: 'Legumes' },
+    { id: '3', value: 'Carne' },
+    { id: '4', value: 'Doces' },
+
 ]
 
 export default function MyList() {
-    return myList.map((item) => {
-        return (
-            <div key={item.id}>
-                <h4>{item.value}</h4>
-            </div>
-        )
-    })
 
-    return myList
+    const [products, setProducts] = useState(myList)
+    const [pesquisa, setPesquisa] = useState('')
+
+    useEffect(
+        () => {
+            if(pesquisa){
+            const newList = myList.filter((item) => {
+                return item.value.toLowerCase().includes(pesquisa.toLowerCase())
+            })
+            setProducts(newList)}
+            else{
+                setProducts(myList)
+            }
+        }, [pesquisa])
+
+    return (
+        <div>
+            <input
+                value={pesquisa}
+                onChange={(e) => setPesquisa(e.target.value)}
+
+                placeholder="Pesquise aqui" type="text" />
+            {products.map((item) => {
+                return (
+                    <div key={item.id}>
+                        <h4>{item.value}</h4>
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
