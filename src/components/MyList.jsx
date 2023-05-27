6016044ca455ba56801709162166c3655a8972c6
RianLimeira <rianlimeira@gmail.com>
2023-05-27 10:17:20 -0300

    update list

--- src/components/MyList.jsx
+++ src/components/MyList.jsx
@@ -1,10 +1,12 @@
-export default function MyList(){
-    const myList = [
-        <h4 key="1">Item1</h4>,
-        <h4 key="2">Item2</h4>,
-        <h4 key="3">Item3</h4>,
-        <h4 key="1">Item4</h4>,
-    ]
+const myList = [
+    <h4 key="1">Item1</h4>,
+    <h4 key="2">Item2</h4>,
+    <h4 key="3">Item3</h4>,
+    <h4 key="4">Item4</h4>,
+]
+
+export default function MyList() {
+
 
     return myList
 }
